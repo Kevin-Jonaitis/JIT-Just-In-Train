422ae9cac7d44bc6aade9e937401dad3a48ad22b
Kevin <kevitis117@gmail.com>
2025-01-20 16:41:58 -0700

    Stable: Pathfinding nodes completed and created

--- Scripts/tracks.gd
+++ Scripts/tracks.gd
@@ -1,16 +1,3 @@
 extends Node2D
 
 class_name Tracks
-
-
-func _on_child_entered_tree(node: Node) -> void:
-	
-	if (node.temp): # We don't care about the user placement track
-		return
-	pass # Replace with function body.
-
-
-func _on_child_exiting_tree(node: Node) -> void:
-	if (node.temp): # We don't care about the user placement track
-		return
-	pass # Replace with function body.
