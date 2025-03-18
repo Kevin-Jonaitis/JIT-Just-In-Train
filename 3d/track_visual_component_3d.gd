d159ccffe7cb1f8a08f22186c6a35c90a6dc2eea
Kevin <kevitis117@gmail.com>
2025-03-18 16:13:40 -0600

    Double up the rails

--- 3d/track_visual_component_3d.gd
+++ 3d/track_visual_component_3d.gd
@@ -9,7 +9,7 @@ var crosstie_distance: float = 7
 # https://www.factorio.com/blog/post/fff-163
 
 #@onready var _crosstie_mesh_instance : MeshInstance2D = $Sprite2D2
-@onready var _crosstie_multimesh : MultiMeshInstance2D = $Crossties
+@onready var _crosstie_multimesh : MultiMeshInstance3D = $Crossties
 #@onready var _curve_points_multimesh : MultiMeshInstance2D = $CurvePoints
 #@onready var _circle_mesh_instance : MeshInstance2D = $Circle
 
@@ -21,14 +21,16 @@ var my_polygon: Array[Vector2] = [
 		Vector2(-0.5,0.5)] #Top Left
 	
 
-@onready var rail : MeshInstance3D = $Rail
+@onready var rail_left : MeshInstance3D = $RailLeft
+@onready var rail_right : MeshInstance3D = $RailRight
 
 @onready var parentTrack : Track = get_parent() as Track
 
 @onready var backing: Line2D = $Backing
-# @onready var rail: Line2D = $Rail
+# @onready var rail_left: Line2D = $Rail
 
 func _ready() -> void:
+	# _crosstie_multimesh.scale = Vector3(0.01, 0.01, 0.01) # make the crossties thinner
 	# TODO: 3D FIX
 	## VERY IMPORTANT: THIS RADIUS MUST BE BIGGER THAN 
 	## THE WIDTH OF 1/2 of TRACK. If it's not, when the mouse gets close to a track
@@ -49,6 +51,57 @@ var drawableFunctionsToCallLater: Array[Callable] = []
 # 	# 	function.call()
 # 	drawableFunctionsToCallLater.clear()
 
+# Offset the path for use by the rails
+func offset_paths(path: Array[Vector2], offset: float) -> Dictionary[String, PackedVector3Array]:
+	var left_path: Array[Vector3] = []
+	var right_path: Array[Vector3] = []
+	var left_vector2: Vector2
+	var right_vector2: Vector2
+	var extra_start_point: Vector2
+	var extra_end_point: Vector2
+	var n: int = path.size()
+	if n == 0:
+		return {"left": left_path, "right": right_path}
+
+	for i: int in range(n):
+		var normal: Vector2 = Vector2.ZERO
+		if i == 0:
+			var dir: Vector2 = (path[1] - path[0]).normalized()
+			normal = Vector2(-dir.y, dir.x)
+			extra_start_point = path[i] - dir * 0.02
+		elif i == n - 1:
+			var dir: Vector2 = (path[i] - path[i - 1]).normalized()
+			normal = Vector2(-dir.y, dir.x)
+			extra_end_point = path[i] + dir * 0.02
+		else:
+			var dir1: Vector2 = (path[i] - path[i - 1]).normalized()
+			var dir2: Vector2 = (path[i + 1] - path[i]).normalized()
+			var normal1: Vector2 = Vector2(-dir1.y, dir1.x)
+			var normal2: Vector2 = Vector2(-dir2.y, dir2.x)
+			normal = (normal1 + normal2).normalized()
+
+		# Add extra point to remove "gaps" bewteen rails		
+		if (i == 0):
+			left_vector2 = extra_start_point + (normal * offset)
+			right_vector2 = extra_start_point - (normal * offset)
+			left_path.append(Vector3(left_vector2.x, 0.001, left_vector2.y))
+			right_path.append(Vector3(right_vector2.x, 0.001, right_vector2.y))
+		
+		left_vector2 = path[i] + normal * offset
+		right_vector2 = path[i] - normal * offset
+		left_path.append(Vector3(left_vector2.x, 0.001, left_vector2.y))
+		right_path.append(Vector3(right_vector2.x, 0.001, right_vector2.y))
+		
+		# Add extra point to remove "gaps" bewteen rails
+		if(i == n - 1):
+			left_vector2 = extra_end_point + (normal * offset)
+			right_vector2 = extra_end_point - (normal * offset)
+			left_path.append(Vector3(left_vector2.x, 0.001, left_vector2.y))
+			right_path.append(Vector3(right_vector2.x, 0.001, right_vector2.y))
+
+	return {"left": left_path, "right": right_path}
+
+
 # Need to update the bezier call here to actually pass in the tangets. Don't feel like doing the rewrite though.
 func update_track_points(points_: Array[Vector2], length: float, get_coord_at_offset: Callable, startTangent: Vector2 = Vector2(0,0), endTangent: Vector2 = Vector2(0,0)) -> void:
 	
@@ -59,18 +112,18 @@ func update_track_points(points_: Array[Vector2], length: float, get_coord_at_of
 	## We add 0.2 pixels to the start and the end of this segment(in the direction it was coming and going) 
 	## so that there's better overlap with the next segment, and we don't get "sliver gaps" between the train paths
 	var draw_points: Array[Vector2] = points_.duplicate()	
-	var last_point_value: Vector2 = points_[points_.size() - 1]
-	draw_points.insert(0, points_[0] - (startTangent * 0.2))
-	draw_points.append(last_point_value - (endTangent * 0.2))
 
-	var vector_3_path: PackedVector3Array = []
-	for point: Vector2 in points_:
-		vector_3_path.append(Vector3(point.x, 0.001, point.y))
-
-	assert(rail.mesh is ArrayMesh, "Rail mesh must be an ImmediateMesh")
-	var mesh: ArrayMesh = rail.mesh as ArrayMesh
-	# assert(rail.mesh is ImmediateMesh, "Rail mesh must be an ImmediateMesh")
-	# var mesh: ImmediateMesh = rail.mesh as ImmediateMesh
+	var results: Dictionary[String, PackedVector3Array] = offset_paths(draw_points, 0.5) # This is just to get the size of the array, we don't need it
+	# var vector_3_path: PackedVector3Array = []
+	var vector_3_path_left: PackedVector3Array = results["left"]
+	var vector_3_path_right: PackedVector3Array = results["right"]
+	
+	assert(rail_left.mesh is ArrayMesh, "Rail mesh must be an ImmediateMesh")
+	assert(rail_right.mesh is ArrayMesh, "Rail mesh must be an ImmediateMesh")
+	var mesh_left: ArrayMesh = rail_left.mesh as ArrayMesh
+	var mesh_right: ArrayMesh = rail_right.mesh as ArrayMesh
+	# assert(rail_left.mesh is ImmediateMesh, "Rail mesh must be an ImmediateMesh")
+	# var mesh: ImmediateMesh = rail_left.mesh as ImmediateMesh
 
 	# var debug_path: Array[Vector3] = [
 	# 	Vector3(2, 0, 2),
@@ -82,8 +135,10 @@ func update_track_points(points_: Array[Vector2], length: float, get_coord_at_of
 
 	#mesh.clear_blend_shapes()
 	
-	mesh.clear_surfaces()
-	TrackDrawer.extrude_polygon_along_path_arraymesh(TrackDrawer.RAIL_POLYGON_VERTICIES, vector_3_path, mesh)
+	mesh_left.clear_surfaces()
+	mesh_right.clear_surfaces()
+	TrackDrawer.extrude_polygon_along_path_arraymesh(TrackDrawer.RAIL_POLYGON_VERTICIES, vector_3_path_left, mesh_left)
+	TrackDrawer.extrude_polygon_along_path_arraymesh(TrackDrawer.RAIL_POLYGON_VERTICIES, vector_3_path_right, mesh_right)
 
 
 	# TrackDrawer.extrude_polygon_along_path(TrackDrawer.RAIL_POLYGON_VERTICIES, vector_3_path, mesh)
@@ -97,7 +152,7 @@ func update_track_points(points_: Array[Vector2], length: float, get_coord_at_of
 
 	#set_points(draw_points)
 	# TODO: 3D fix
-	# rail.set_points(draw_points)
+	# rail_left.set_points(draw_points)
 	# backing.set_points(draw_points)
 	_update_crossties(length, get_coord_at_offset)
 	# for point: Vector2 in draw_points: 
@@ -106,8 +161,7 @@ func update_track_points(points_: Array[Vector2], length: float, get_coord_at_of
 	# queue_redraw()
 
 func make_track_invisible() -> void:
-	# TODO: 3D FIX
-	# _crosstie_multimesh.multimesh.instance_count = 0
+	_crosstie_multimesh.multimesh.instance_count = 0
 	pass
 
 
@@ -124,10 +178,27 @@ func _update_crossties(path_length: float, get_coord_at_offset: Callable) -> voi
 
 	crossties.instance_count = crosstie_count
 	
+	# for i: int in range(crosstie_count):
+	# 	var t: Transform2D = Transform2D()
+	# 	var crosstie_position: Vector2 = get_coord_at_offset.call((i * crosstie_distance) + crosstie_distance / 2.0)
+	# 	var next_position: Vector2 = get_coord_at_offset.call((i + 1) * crosstie_distance)
+	# 	t = t.rotated((next_position - crosstie_position).normalized().angle())
+	# 	t.origin = crosstie_position
+	# 	crossties.set_instance_transform_2d(i, t)
+
 	for i: int in range(crosstie_count):
-		var t: Transform2D = Transform2D()
+		var t: Transform3D = Transform3D()
 		var crosstie_position: Vector2 = get_coord_at_offset.call((i * crosstie_distance) + crosstie_distance / 2.0)
 		var next_position: Vector2 = get_coord_at_offset.call((i + 1) * crosstie_distance)
-		t = t.rotated((next_position - crosstie_position).normalized().angle())
-		t.origin = crosstie_position
-		crossties.set_instance_transform_2d(i, t)
+		print("POSITION:" + str(crosstie_position))
+		# t = t.rotated(Vector3(next_position.x, 0, next_position.y) - Vector3(crosstie_position.x, 0, crosstie_position.y))
+		# t.origin = 
+		t.basis = Basis().scaled(Vector3(0.01, 0.01, 0.01)).rotated(Vector3(1, 0, 0), PI / 2)
+		# .rotated(Vector3(0, 1, 0), (next_position - crosstie_position).normalized().angle())
+		t.origin = Vector3(crosstie_position.x, 0 ,crosstie_position.y)
+
+		# t = t.scaled(Vector3(0.01, 0.01, 0.01)) # make the crossties thiner
+		# t.origin = Vector3(crosstie_position.x, 0 ,crosstie_position.y)
+		# var transform: Transform3D = Transform3D(t, Vector3(crosstie_position.x, 0 ,crosstie_position.y))
+		print("TRANSFORM:", t)
+		crossties.set_instance_transform(i, t)
